c590778d7f402a57bf79039dc500de418feeda8c
Hp <alfi19bachtiar@gmail.com>
2020-07-26 19:41:31 +0700

    First Commit

--- app/src/main/java/com/ngoding/githubuserapp/activity/FavoriteActivity.java
+++ app/src/main/java/com/ngoding/githubuserapp/activity/FavoriteActivity.java
@@ -18,6 +18,7 @@ import com.google.android.material.floatingactionbutton.ExtendedFloatingActionBu
 import com.ngoding.githubuserapp.R;
 import com.ngoding.githubuserapp.adapter.FavoriteAdapter;
 import com.ngoding.githubuserapp.viewmodel.FavoriteViewModel;
+import com.ngoding.githubuserapp.widget.FavoritesWidget;
 
 public class FavoriteActivity extends AppCompatActivity {
 
@@ -100,6 +101,7 @@ public class FavoriteActivity extends AppCompatActivity {
         fabDeleteAll.setOnClickListener(v -> {
             if (isOpen) {
                 favoriteViewModel.deleteAllFavorites();
+                FavoritesWidget.sendRefreshBroadcast(FavoriteActivity.this);
                 Toast.makeText(FavoriteActivity.this, R.string.txt_all_favorites_deleted, Toast.LENGTH_SHORT).show();
                 fabDeleteAll.startAnimation(animFabClose);
                 fabDeleteSweep.startAnimation(animFabClose);
@@ -122,6 +124,7 @@ public class FavoriteActivity extends AppCompatActivity {
             @Override
             public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
                 favoriteViewModel.delete(favoriteAdapter.getFavoriteAt(viewHolder.getAdapterPosition()));
+                FavoritesWidget.sendRefreshBroadcast(FavoriteActivity.this);
                 Toast.makeText(FavoriteActivity.this, R.string.txt_favorite_deleted, Toast.LENGTH_SHORT).show();
             }
         }).attachToRecyclerView(recyclerView);
